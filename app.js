27ef8663e3a5c87ff60ea05108f1974c279885b3
Tuyet Le <toowee.le@gmail.com>
2020-03-10 18:30:37 +0000

    Get the weather data from the Dark Sky API

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ window.addEventListener('load', () => {
         navigator.geolocation.getCurrentPosition(position => {
             longitude = position.coords.longitude;
             latitude = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/cb345289941bd4c429c64c55a6af659f/${latitude},${longitude}`;
+
+            // Get the information from the weather API server
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
         });
     } else {
         alert("Geolocation is not supported by this browser.");
