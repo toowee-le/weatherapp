35bd9dc5ce020043ac05c6330931de45c2b796fb
Tuyet Le <toowee.le@gmail.com>
2020-03-10 20:47:24 +0000

    Set weather conditions DOM elements to JSON data

--- app.js
+++ app.js
@@ -2,31 +2,33 @@ window.addEventListener('load', () => {
     let longitude;
     let latitude;
 
-    let currentTemp = document.getElementById('degree');
-    let description = document.getElementById('description');
-    let currentLocation = document.getElementById('location');
-
     // Get the latitude and longitude of the user's location
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             longitude = position.coords.longitude;
             latitude = position.coords.latitude;
 
+            // To avoid the cors issue, run the API through a proxy
             const proxy = "https://cors-anywhere.herokuapp.com/";
             const api = `${proxy}https://api.darksky.net/forecast/ef062569720f0a4a50be50c0492a8d82/${latitude},${longitude}`;
 
-            // Get the information from the weather API server
+            // Fetch the weather information from the Dark Sky API server
             fetch(api)
             .then(response => {
                 return response.json();
             })
             .then(data => {
                 console.log(data);
-                // Set DOM elements to API data
-                const { temperature, summary, } = data.currently;
-                currentTemp.textContent = Math.floor(temperature);
-                description.textContent = summary;
-                currentLocation.textContent = data.timezone;
+                const { temperature, summary, icon, humidity, windSpeed, precipProbability, pressure } = data.currently;
+
+                // Set DOM elements to JSON/API data
+                document.getElementById('location').textContent = data.timezone;
+                document.getElementById('degree').textContent = Math.floor(temperature);
+                document.getElementById('description').textContent = summary;
+                document.getElementById('humidity').textContent = `Humidity: ${Math.floor(humidity*100)}%`;
+                document.getElementById('wind').textContent = `Wind: ${windSpeed} mph`;
+                document.getElementById('precipitation').textContent = `Precipitation: ${precipProbability}%`;
+                document.getElementById('pressure').textContent = `Pressure: ${pressure} mb`;                
             })
         });
     } else {
